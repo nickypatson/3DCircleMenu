a7935a9446b7e0950c42257648bb3451ccaee331
nickypatson <nickypatson@gmail.com>
2018-03-02 14:29:10 +0530

    Initial Commit

---
+++ testingTabBarView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testingTabBarView
+//
+//  Created by Nick on 02/03/18.
+//  Copyright © 2018 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
