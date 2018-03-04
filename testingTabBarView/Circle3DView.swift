73f5126ac7a41c45b7bf54094dc42ce3ca095aee
nickypatson <nickypatson@gmail.com>
2018-03-04 17:32:35 +0530

    first commit

---
+++ testingTabBarView/Circle3DView.swift
@@ -0,0 +1,21 @@
+//
+//  Circle3DView.swift
+//  testingTabBarView
+//
+//  Created by Nick on 04/03/18.
+//  Copyright © 2018 Nick. All rights reserved.
+//
+
+import UIKit
+
+class Circle3DView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
